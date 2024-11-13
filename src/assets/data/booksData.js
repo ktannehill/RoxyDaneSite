0b0159d13d986b4a761e7d8d6fa94d4646838172
Kat Tannehill <katetannehill@gmail.com>
2024-11-13 12:13:07 -0700

    Start data file, import images

---
+++ src/assets/data/booksData.js
@@ -0,0 +1,46 @@
+import kidslearningscissor from "../images/books/kids-learning-scissor.jpg"
+import kidslearningtracing from "../images/books/kids-learning-tracing.jpg"
+import kidslearningwrite from "../images/books/kids-learning-write.jpg"
+import kidscoloringdinosaur from "../images/books/kids-coloring-dinosaur.jpg"
+import kidscoloringmonstertruck from "../images/books/kids-coloring-monster-truck.jpg"
+import kidscoloringprincess from "../images/books/kids-coloring-princess.jpg"
+import kidscoloringprincess2 from "../images/books/kids-coloring-princess-vol2.jpg"
+import kidscoloringunicorn from "../images/books/kids-coloring-unicorn.jpg"
+import kidsbookstar from "../images/books/kids-book-star.jpg"
+import watercolorbotanical from "../images/books/watercolor-botanical.jpg"
+import watercolorbutterflies from "../images/books/watercolor-butterflies.jpg"
+import watercolorcars from "../images/books/watercolor-cars.jpg"
+import watercolorcats from "../images/books/watercolor-cats.jpg"
+import watercolordogs from "../images/books/watercolor-dogs.jpg"
+import watercolordogs2 from "../images/books/watercolor-dogs-vol2.jpg"
+import watercoloreuropean from "../images/books/watercolor-european.jpg"
+import watercolorfloral from "../images/books/watercolor-floral.jpg"
+import watercolorgarden from "../images/books/watercolor-garden.jpg"
+import watercolorhorses from "../images/books/watercolor-horses.jpg"
+import watercolorhorses2 from "../images/books/watercolor-horses-vol2.jpg"
+import watercolorlandscape from "../images/books/watercolor-landscape.jpg"
+import watercolorlandscape2 from "../images/books/watercolor-landscape-vol2.jpg"
+import watercolorseascape from "../images/books/watercolor-seascape.jpg"
+import watercolorseascape2 from "../images/books/watercolor-seascape-vol2.jpg"
+import watercolorvillage from "../images/books/watercolor-village.jpg"
+import coloringsugarskulls from "../images/books/coloring-sugar-skulls.jpg"
+import affirmationempower from "../images/books/affirmation-empower.jpg"
+import inkhappymotivation from "../images/books/inkhappy-motivation.jpg"
+import inkhappyrelax from "../images/books/inkhappy-relax.jpg"
+import inkhappyrelax2 from "../images/books/inkhappy-relax-vol2.jpg"
+import inkhappyrenew from "../images/books/inkhappy-renew.jpg"
+import inkhappystress from "../images/books/inkhappy-stress.jpg"
+
+const books = [
+    {
+        id: 1,
+        title: "Scissor Skills Activity Book For Kids",
+        image: kidslearningscissor,
+        type: "kids",
+        category: "learning",
+        description: "A short description of Book Title 1.",
+        link: "https://www.amazon.com/Scissor-Skills-Activity-Book-Kids/dp/B0DG61JD8Y?ref_=ast_author_dp_rw&dib=eyJ2IjoiMSJ9.ok4r3sFTQZJMW-OxlJfJANdcTJOQ3Jd_YaRtll7XWvx13QGkIZROZTXKfoSzvRgjrZMHDLpxRMxt8VUsJcKity1u0Xi4qXpqW-PNxJw8McnntoIe4cF7PyUcntaGtJIkRC0RBZPS_mRO_hWwBLJu1CuajYnyMLJBanUEqgUu-pWIuN-FSg3XJkxxvhMTkIMQjXXzhTzhTCapJL73sFewRkiYpy8vTDkbxFg1GPcwPNY.7LWvnMB4hueLuNL3x6vqYGK9ujFMvoT9qZzOX_oJ_u4&dib_tag=AUTHOR"
+    },
+]
+
+export default books;
