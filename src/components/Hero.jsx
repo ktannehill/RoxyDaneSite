f80e47d1c09dc04ee865e996fbc507910b38577c
Kat Tannehill <katetannehill@gmail.com>
2024-11-14 10:51:37 -0700

    Base styling for Hero banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,16 @@
+import "../styles/hero.css"
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Roxy Dane</h1>
-      <p>
-        Doggo ipsum doge h*ck shibe smol very jealous pupper borking doggo pats
-      </p>
-    </div>
+    <section id="hero">
+      <img src="https://images.unsplash.com/photo-1694370806388-e372ee118336?q=80&w=2015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Watercolor landscape from Birmingham Museums Trust" />
+      <div>
+        <h1>Roxy Dane</h1>
+        <p>
+          Doggo ipsum doge h*ck shibe smol very jealous pupper borking doggo pats
+        </p>
+      </div>
+    </section>
   )
 }
 
