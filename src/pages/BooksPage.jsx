da93f5820873667ad45d30efb3ddf8134f58b3a3
Kat Tannehill <katetannehill@gmail.com>
2024-11-19 15:19:07 -0700

    Restructure booksData content, stremline type & category

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+import booksData from '../assets/data/booksData'
+import Card from '../components/Card'
+import '../styles/sections.css'
+import { useEffect } from 'react'
 
 const BooksPage = () => {
+  const { type } = useParams()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(type !== "kids" && type !== "teensadults") {
+      navigate("/")
+    }
+    
+  }, [type])
+
+  const books = booksData.filter(book => book.type === type)
+  
+  const categories = [...new Set(books.map(book => book.category))]
+
+  console.log(categories)
+
   return (
-    <div>BooksPage</div>
+    <div className='container'>
+      <h1>{type === "kids" ? "Kids" : "Teens and Adults"} Books</h1>
+    </div>
   )
 }
 
