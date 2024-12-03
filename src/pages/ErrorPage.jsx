637d3cd2be5bb94c47da4ab0eb9c33dcb5e24dc0
Kat Tannehill <katetannehill@gmail.com>
2024-12-03 14:52:33 -0700

    Error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <div id="flex">
+      <Header />
+      <div id="outlet">
+        <div className='container'>
+          <h2>
+            Oops!
+          </h2>
+          <p>
+            This page hasn't been colored in yet!
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
