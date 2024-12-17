4fdcc5d2ca2fe669328121c61d649303d336d1cf
Kat Tannehill <katetannehill@gmail.com>
2024-12-17 11:29:23 -0700

    Open book links in new tab

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({ image, subtitle, description, link, dark }) => {
       <div className={`${dark ? "" : "content"}`}>
         <h3>{subtitle}</h3>
         <p>{description}</p>
-        {link?.length > 0 && <Link to={link}><Button text="View more" /></Link>}
+        {link?.length > 0 && <Link to={link} target="_blank" rel="noopener noreferrer"><Button text="View more" /></Link>}
       </div>
     </article>
   )
