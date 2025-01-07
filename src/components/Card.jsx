ece09734b125499caaa5b40025aac76e955a6ac1
Kat Tannehill <katetannehill@gmail.com>
2025-01-07 16:00:23 -0700

    Link books images, refactor button link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,11 +6,20 @@ const Card = ({ image, subtitle, description, link, dark }) => {
 
   return (
     <article>
-      <img src={image} alt={description} loading="lazy" />
+      {link?.length ? (
+        <div className={`${link ? "book-cover" : ""}`}>
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <img src={image} alt={description} loading="lazy" />
+            <span>View on Amazon</span>
+          </a>
+        </div>
+      ) : (
+        <img src={image} alt={description} loading="lazy" />
+      )}
       <div className={`${dark ? "" : "content"}`}>
         <h3>{subtitle}</h3>
         <p>{description}</p>
-        {link?.length > 0 && <Link to={link} target="_blank" rel="noopener noreferrer"><Button text="View more" /></Link>}
+        {/* {link?.length > 0 && <Link to={link} target="_blank" rel="noopener noreferrer"><Button text="View more" /></Link>} */}
       </div>
     </article>
   )
